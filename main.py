6059411d589847f36c484b7f0bc0a71103a1d52a
ctocCyient <48665615+ctocCyient@users.noreply.github.com>
2019-08-16 13:45:20 +0100

    IoT

---
+++ main.py
@@ -0,0 +1,34 @@
+import sensor_data as IoT
+import simplejson as json
+import requests
+import time;
+
+data={}
+# the result is a Python dictionary:
+server_data = []
+with open('example_1.json') as f:
+  data = json.load(f)
+
+for (k, v) in data.items():
+
+
+   if(k!="configuration"):
+       if(v.get("DeviceType")=="Analog"):
+           device_data = {}
+           value = IoT.read_data_rs485_holding(data['configuration'],v)
+           if v.get("Threshold")==value:
+               print("send alaram")
+           else:
+               device_data['Gatewayname']=v.get("Gatewayname")
+               device_data['parameter']=k
+               device_data['timestamp']=time.time()
+               device_data['value']=value
+               server_data.append(device_data)
+
+
+
+
+
+
+
+#IoT.read_data_rs485(data['configuration'],data['Temperature_Temperature'])
